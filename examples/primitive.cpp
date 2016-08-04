a3aba9b3517af55366d4ed9238f1e49f048595af
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-08-04 23:03:59 +0900

    [add] Make it support the demo to compare formats

--- examples/primitive.cpp
+++ examples/primitive.cpp
@@ -9,19 +9,29 @@
 #include <iostream>
 
 #include <image/BitmapImage.hpp>
+#include <image/JpegImage.hpp>
 #include <type/NormalizedColor.hpp>
 
 int main(int argc, const char * argv[])
 {
     constexpr unsigned int kRadius = 20;
     fj::BitmapImage image(640, 480);
+    fj::JpegImage jpeg;
+    jpeg.initialize(640, 480);
     
     image.drawCircle(image.getWidth() / 0.6, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::RED);
     image.drawCircle(image.getWidth() / 2.0, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::WHITE);
     image.drawCircle(image.getWidth() / 3.0, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::BLUE);
     image.drawCircle(image.getWidth() / 6.0, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::GREEN);
+
+    jpeg.drawCircle(image.getWidth() / 0.6, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::RED);
+    jpeg.drawCircle(image.getWidth() / 2.0, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::WHITE);
+    jpeg.drawCircle(image.getWidth() / 3.0, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::BLUE);
+    jpeg.drawCircle(image.getWidth() / 6.0, image.getHeight() / 2.0, kRadius, fj::NormalizedColor::GREEN);
+
     
     image.saveToFile("primitive.bmp");
+    jpeg.saveToFile("primitive.jpg");
     
     return EXIT_SUCCESS;
 }
