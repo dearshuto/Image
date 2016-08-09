a0c9adcc960cac760c14176d3b1a370b6bb76a0d
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-08-09 17:28:44 +0900

    [modify] Make libjpeg generate static lib

--- cmake_generator.py
+++ cmake_generator.py
@@ -14,11 +14,11 @@ def cmake_setting():
 	cmd = ['cmake']
 	if me == 'Windows':
 		print 'run cmake as Windows'	
-		cmd = ['cmake', '..', '-G', 'Visual Studio 14 2015']
+		cmd = ['cmake', '..', '-G', 'Visual Studio 14 2015', '-DBUILD_STATIC=ON -DBUILD_SHARED_LIBS=OF)']
 	elif me == 'Linux':
-		cmd = ['cmake', '..', '-G', 'Makefile']
+		cmd = ['cmake', '..', '-G', 'Makefile', '-DBUILD_STATIC=ON -DBUILD_SHARED_LIBS=OF']
 	elif me == 'Darwin':
-		cmd = ['cmake', '..', '-G', 'Xcode']
+		cmd = ['cmake', '..', '-G', 'Xcode', '-DBUILD_STATIC=ON -DBUILD_SHARED_LIBS=OF)']
 
 	# run cmake with arguments
 	try:
